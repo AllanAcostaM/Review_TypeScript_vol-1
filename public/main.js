33f92beae13efdef55fc7fc2e6806014c11e91ba
Jossep Acosta <allanacost.27@gmail.com>
2025-07-16 20:46:39 -0500

    functions

--- public/main.js
+++ public/main.js
@@ -1,25 +1,14 @@
 "use strict";
-// tipos de datos avanzados 
-// Union:
-let id = "plomo es lo que hay, plomo es lo que viene";
-id = 12344;
-let userId = "plomo es lo que hay, plomo es lo que viene!!!";
-let myArr = [10, 20, 30];
-myArr = [40, 50, "str1", "str2", true, false,];
-let stateloading = "loading";
-let stateSecond = "error";
-let state = "error";
-const esdla = {
-    title: "el señor de los anillos",
-    duration: 240,
-    finalContinue: "true",
-};
-const harryPotter = {
-    title: "Harry Potter",
-    author: "matarife",
-    duration: 220,
-    pages: 150,
-    finalContinue: true
-};
-console.log(esdla);
-console.log(harryPotter);
+// Funciones
+function sum(n1, n2) {
+    return n1 + n2;
+}
+let result = sum(3, 4);
+console.log(result);
+const toUpper = (str) => str.toUpperCase();
+let str = toUpper("hello");
+console.log(str);
+function sayHello() {
+    console.log("hello!!");
+}
+sayHello();
--- src/main.ts
+++ src/main.ts
@@ -1,66 +1,22 @@
-// tipos de datos avanzados 
+// Funciones
 
-
-// Union:
-
-let id: string | number = "plomo es lo que hay, plomo es lo que viene"
-id = 12344;
-
-type id = number | string;
-
-let userId: id = "plomo es lo que hay, plomo es lo que viene!!!";
-
-let myArr: (number | string | boolean) [] = [10, 20, 30];
-
-myArr = [40, 50, "str1", "str2", true, false,]
-
-// literal
-
-type stateLoading = "loading";
-type stateError = "error"
-
-let stateloading: stateLoading = "loading";
-let stateSecond: stateError = "error"; 
-
-
-type state = stateLoading | stateError 
-
-let state: state = "error";
-
-
-type movie = {
-    title: string;
-    duration: number;
-    finalContinue: boolean | string;
+function sum(n1: number, n2: number) {
+    return n1 + n2;
 }
 
-const esdla: movie = {
-    title: "el señor de los anillos",
-    duration: 240,
-    finalContinue: "true",
+let result = sum(3, 4)
+console.log(result);
+
+
+
+const toUpper = (str: string) => str.toUpperCase();
+let str = toUpper("hello");
+console.log(str);
+
+
+function sayHello() {
+    console.log("hello!!");
 }
 
-// Interseccion
-
-type book = {
-    title: string,
-    author: string,
-    pages: number,
-}
-
-type bookAdaptation = movie & book;
-
-const harryPotter: bookAdaptation = {
-    title: "Harry Potter",
-    author: "matarife",
-    duration: 220,
-    pages: 150,
-    finalContinue: true
-}
-
-console.log(esdla);
-
-
-console.log(harryPotter);
-
+sayHello();
 
